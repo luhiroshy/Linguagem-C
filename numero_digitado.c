3cddef088768de44c3cf683d035ca42aa339a359
Luan Hiroshy <164904163+luhiroshy@users.noreply.github.com>
2025-05-29 16:32:37 -0300

    Update and rename Número_digitado.c to numero_digitado.c

--- numero_digitado.c
+++ numero_digitado.c
@@ -6,7 +6,7 @@ int main(void) {
     printf("Insira um número: ");
     scanf("%d", &n);
     
-    printf("O número digitado foi: %d", n);
+    printf("O número digitado foi: %d!", n);
     
     return 0;
 }
