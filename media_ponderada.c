344dd8b031b31cb865d8d5a47ac899b192c20169
Luan Hiroshy <164904163+luhiroshy@users.noreply.github.com>
2025-05-29 17:06:17 -0300

    Update media_ponderada.c

--- media_ponderada.c
+++ media_ponderada.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     float n1, n2, n3, n4, n5, p1, p2, p3, p4, p5, pesos, media;
     
     printf ("Digite cinco números e descubra a média ponderada \n");
