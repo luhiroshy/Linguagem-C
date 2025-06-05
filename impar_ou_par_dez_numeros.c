d2907ff9298597c5b81f3354f572a19dac0aefd7
Luan Hiroshy <164904163+luhiroshy@users.noreply.github.com>
2025-06-05 12:55:03 -0300

    Create impar_ou_par_dez_numeros.c

---
+++ impar_ou_par_dez_numeros.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    printf("Digite dez números e descubra se são ímpares ou pares\n\n");
+    printf("Digite o primeiro número: ");
+    scanf("%d", &i);
+    
+    if(i % 2 == 0){
+            printf("O número %d é par\n\n", i);
+        } else {
+            printf("O número %d é ímpar\n\n", i);
+        }
+    
+    for(i = 1;i <= 9;i++) {
+        printf("Insira um número: ");
+        scanf("%d", &i);
+        if(i % 2 == 0){
+            printf("O número %d é par\n\n", i);
+        } else {
+            printf("O número %d é ímpar\n\n", i);
+        }
+    }
+    
+
+    return 0;
+}
